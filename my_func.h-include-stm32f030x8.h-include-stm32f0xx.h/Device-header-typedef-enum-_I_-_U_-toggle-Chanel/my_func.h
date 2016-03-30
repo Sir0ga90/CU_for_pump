3ad95adc57971a6fa6a276c113681fb937a20497
Sir0ga90 <1sergey99chursin0@gmail.com>
2016-03-30 17:36:26 +0300

    Delete my_func.h

--- my_func.h-include-stm32f030x8.h-include-stm32f0xx.h/Device-header-typedef-enum-_I_-_U_-toggle-Chanel/my_func.h
+++
@@ -1,9 +0,0 @@
-#include "stm32f030x8.h"
-#include "stm32f0xx.h"                  // Device header
-
-typedef enum {_I_, _U_, toggle}Chanel;      // type of ADC input
-
-uint32_t calculate_val(uint32_t adc_val);
-
-void dig_to_disp(uint32_t out_dig);
-
